0a0cb474f7d8754420def0b25789275f9c17e000
reubenc-bit <69934343+reubenc-bit@users.noreply.github.com>
2025-08-24 17:25:29 +0200

    Delete api/index.js

--- api/index.js
+++
@@ -1,8 +0,0 @@
-// Simple API handler for Vercel
-export default async function handler(req, res) {
-  // Import the built server
-  const { default: app } = await import('../dist/index.js');
-  
-  // Handle the request
-  return app(req, res);
-}
