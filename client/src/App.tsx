7dab8631b8af4451790283ecbf0183ee2db70482
reubenc-bit <69934343+reubenc-bit@users.noreply.github.com>
2025-08-26 19:18:51 +0200

    Add files via upload

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -129,7 +129,7 @@ function AppContent() {
           href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.0/css/all.min.css" 
         />
         
-        <EnhancedNavigation />
+        <EnhancedNavigation onAuthModalOpen={openAuthModal} />
         
         {renderMainContent()}
 
